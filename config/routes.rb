bf31cd3c43dbd7fbdef5e71fbd33a4c66f03c55c
Gabriel <gabrielvieiradw@gmail.com>
2021-10-28 10:56:36 -0300

    Routes config

--- config/routes.rb
+++ config/routes.rb
@@ -1,49 +1,22 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      get 'dashboards/index'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      get 'recommendations/index'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      get 'executions/create'
-      get 'executions/update'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      get 'sitcoms/show'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      get 'movies/show'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      get 'searches/index'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      get 'rates/index'
-      get 'rates/create'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      get 'favorites/index'
-      get 'favorites/create'
-      get 'favorites/destroy'
-    end
-  end
   root to: "welcome#index"
   devise_for :users
 
+  namespace :api do
+    namespace :v1 do
+      get 'dashboards', to: 'dashboards#index', as: 'dashboard'
+      resources :favorites, only: %i( index create )
+      delete 'my_list/:type/:id', to: 'favorites#destroy'
+      resources :rates, only: [:index, :create]
+      resources :searches, only: :index
+      resources :sitcoms, only: :show
+      resources :movies, only: :show do
+        member do
+          get 'executions', to: 'executions#show'
+          get 'executions', to: 'executions#update'
+        end
+      end
+      resources :recommendations, only: :index
+    end
+  end
 end
